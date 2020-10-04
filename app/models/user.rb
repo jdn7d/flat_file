997253109a3d104bac527ded45e241126f98adeb
Jessie <jssdnvn.com>
2020-10-03 22:28:32 -0500

    added s to validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
     validates :email, presence: true
     validates :email, uniqueness: true
-    validate :password, presence: true, length: {minimum: 6}
+    validates :password, presence: true, length: {minimum: 6}
 
     has_many :artists
     has_many :artworks, through: :artists
